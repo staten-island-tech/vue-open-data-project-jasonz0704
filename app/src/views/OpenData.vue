16ef9366250f96820bf33e0f7eab5f6a4f8e9232
jasonz0704 <oonetwome@gmail.com>
2026-03-18 14:00:53 -0400

    2

--- app/src/views/OpenData.vue
+++ app/src/views/OpenData.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
-    <h1>hi bro</h1>
+    <h1>here is the data for H2O consumed by the yorkers</h1>
+    <DataCard v-for="water in waterconsumption" :key="water.name" :water="water"></DataCard>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import DataCard from '@/components/DataCard.vue'
+
+const APIURL = 'https://data.cityofnewyork.us/resource/ia2d-e54m.json'
+
+const waterconsumption = ref([])
+
+async function getData(APIURL) {
+  try {
+    const response = await fetch(APIURL)
+    const data = await response.json()
+
+    waterconsumption = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+getData(APIURL)
+</script>
 
 <style scoped></style>
