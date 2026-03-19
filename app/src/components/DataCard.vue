17dc29507b1ba992b8b6c1da691a828d90bb196c
jasonz0704 <oonetwome@gmail.com>
2026-03-19 11:02:24 -0400

    3

--- app/src/components/DataCard.vue
+++ app/src/components/DataCard.vue
@@ -1,7 +1,33 @@
 <template>
-  <div></div>
+  <router-link :to="wtrdataPath" class="card">
+    <h2>{{ waterconsumption.name }}</h2>
+  </router-link>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  waterconsumption: {
+    type: Object,
+    required: true,
+  },
+})
 
-<style scoped></style>
+const wtrdataPath = computed(() => {
+  return `/wtrdata/${props.waterconsumption.name}`
+})
+</script>
+
+<style scoped>
+.card {
+  width: 28%;
+  height: 500px;
+  background-color: aliceblue;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  text-transform: uppercase;
+}
+</style>
--- app/src/views/OpenData.vue
+++ app/src/views/OpenData.vue
@@ -1,29 +1,42 @@
 <template>
-  <div>
-    <h1>here is the data for H2O consumed by the yorkers</h1>
-    <DataCard v-for="water in waterconsumption" :key="water.name" :water="water"></DataCard>
+  <h1>here is the data for H2O consumed by the yorkers</h1>
+  <div class="container">
+    <DataCard v-for="water in waterconsumption" :key="water.name" :waterconsumption="water" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import DataCard from '@/components/DataCard.vue'
 
-const APIURL = 'https://data.cityofnewyork.us/resource/ia2d-e54m.json'
-
 const waterconsumption = ref([])
-
-async function getData(APIURL) {
+async function getData() {
   try {
-    const response = await fetch(APIURL)
+    const response = await fetch(
+      'https://data.cityofnewyork.us/resource/ia2d-e54m.json?limit=150&offset=0',
+    )
     const data = await response.json()
 
-    waterconsumption = data
+    waterconsumption.value = data.year
   } catch (error) {
     console.log(error)
   }
 }
-getData(APIURL)
+onMounted(() => {
+  getData()
+})
+
+console.log(waterconsumption)
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 80vw;
+  margin: 30px auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
